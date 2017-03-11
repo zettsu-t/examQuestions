b5059fcf59aba48b388368b242873ae645d86230
Zettsu Tatsuya <zettsu.tatsuya@gmail.com>
2017-03-11 10:44:54 +0900

    Measure Haskell code

--- 2008math4.hs
+++ 2008math4.hs
@@ -1,6 +1,8 @@
 -- 麻布中学校 2008年 入試問題 算数 問4 解法
 
 import Data.List
+import Data.MultiMap
+import System.Environment (getArgs)
 
 -- 演算子
 opToChar 0 = "+"
@@ -13,18 +15,47 @@ apply (x:xs) _ = x
 
 makePair nums ops = (value, str)
   where value = apply nums ops
-        str = concat $ concat $ transpose [map show nums, map opToChar ops ++ [""]]
+        str = concat $ concat $ transpose [Data.List.map show nums, Data.List.map opToChar ops ++ [""]]
 
-allLists minNum maxNum = map f opLists
+allLists minNum maxNum = Data.List.map f opLists
   where nums = [minNum..maxNum]
         opLists = sequence (replicate (maxNum - minNum) [0,1])
         f ops = makePair nums ops
 
+allListMap minNum maxNum = Data.MultiMap.fromList $ allLists minNum maxNum
+
 -- 問4-1
 q1 = sort $ allLists 1 4
 -- 問4-2
-q2 = [(snd p, snd q) | p <- (allLists 1 5), q <- (allLists 2 6), fst p == fst q]
+q2full leftMin leftMax rightMin rightMax =
+  [(snd p, snd q) | p <- (allLists leftMin leftMax), q <- (allLists rightMin rightMax), fst p == fst q]
 
-main = do
+q2map leftMin leftMax rightMin rightMax =
+  Data.List.concatMap f4 $ filter f3 $ Data.List.map f2 $ Data.List.map f1 (allLists leftMin leftMax)
+  where f1 l = (l, Data.MultiMap.lookup (fst l) (allListMap rightMin rightMax))
+        f2 (l,r) = (snd l, r)
+        f3 (l,r) = (not $ Data.List.null r)
+        f4 (l,r) = [(l, x) | x <-r]
+
+q2 = q2full 1 5 2 6
+
+paramSet (mode:lMin:lMax:rMin:rMax:xs) =
+  (True, mode, read lMin :: Int, read lMax :: Int, read rMin :: Int, read rMax :: Int)
+paramSet _ = (False, "", 0, 0, 0, 0)
+
+solveAll :: IO ()
+solveAll = do
   putStrLn $ show q1
   putStrLn $ show q2
+
+solve :: String -> Int -> Int -> Int -> Int -> IO ()
+solve mode leftMin leftMax rightMin rightMax = do
+  putStrLn $ show $ f leftMin leftMax rightMin rightMax
+  where f = if (mode == "map") then q2map else q2full
+
+main = do
+  args <- getArgs
+  let (fixed, mode, leftMin, leftMax, rightMin, rightMax) = paramSet args
+  case fixed of
+    False -> solveAll
+    True  -> solve mode leftMin leftMax rightMin rightMax
